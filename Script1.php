7add0dc938d422cecb0d1c35bc7ce15665c2cfba
Soufiane Akif <75849464+Soufianeakif@users.noreply.github.com>
2022-04-09 09:59:26 +0000

    Update Script1.php

--- Script1.php
+++ Script1.php
@@ -1,12 +1,24 @@
 <?php
-$servername = "0.0.0.0";
+$servername = "localhost";
 $username = "root";
-$password = "123";
+$password = "";
+$dbname = "test00";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected";
+
+$sql = "SELECT * FROM t00";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["nom"]. " " . $row["pass"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
 ?>
